cb5e65e2b0f280733f5374c0215fbf31cc6435b7
samifalouti1 <samifalouti01@yahoo.com>
2025-03-20 05:14:52 +0100

    Update

--- src/pages/LandingPage/Users.js
+++ src/pages/LandingPage/Users.js
@@ -9,7 +9,7 @@ const Users = () => {
     useEffect(() => {
         if (inView) {
             let start = 0;
-            const end = 14329; // Set your static number
+            const end = 14342; // Set your static number
             const duration = 2000; // Animation duration in ms
             const increment = Math.ceil(end / (duration / 16)); 
 
